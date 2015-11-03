d0311e3ad411bb727f279e69a00b40d0b9c52086
Vali Malinoiu <0x4139@gmail.com>
2015-11-03 18:16:37 +0200

    another way to limit the queue

--- publisher.go
+++ publisher.go
@@ -14,14 +14,17 @@ type Publisher struct {
 
 
 func NewPublisher(config Config) Publisher {
-	redisPool := redis.NewPool(func() (redis.Conn, error) {
-		c, err := redis.Dial("tcp", config.RedisAddress)
-		if err != nil {
-			panic(err)
-		}
-		return c, nil
-	}, config.MaxConnections)
-
+	redisPool := &redis.Pool{
+		MaxIdle: 10,
+		MaxActive: config.MaxConnections, // max number of connections
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", config.RedisAddress)
+			if err != nil {
+				panic(err.Error())
+			}
+			return c, err
+		},
+	}
 	return Publisher{
 		connectionPool :redisPool,
 	}
--- subscriber.go
+++ subscriber.go
@@ -31,13 +31,17 @@ func (subscriber *Subscriber) Close() {
 	subscriber.connectionPool.Close()
 }
 func NewSubscriber(config Config) Subscriber {
-	redisPool := redis.NewPool(func() (redis.Conn, error) {
-		c, err := redis.Dial("tcp", config.RedisAddress)
-		if err != nil {
-			panic(err)
-		}
-		return c, nil
-	}, config.MaxConnections)
+	redisPool := &redis.Pool{
+		MaxIdle: 10,
+		MaxActive: config.MaxConnections, // max number of connections
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", config.RedisAddress)
+			if err != nil {
+				panic(err.Error())
+			}
+			return c, err
+		},
+	}
 
 	return Subscriber{
 		connectionPool :redisPool,
